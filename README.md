be5b899f76c7951fc1ec394cfa1e1e4e5c869a08
Ole Henrik Stabell <hennamann9999@gmail.com>
2017-08-19 00:05:27 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# Adobe Story Desktop App
+
+This is an unoffical desktop app for the new HTML5 version of Adobe Story. It´s built using Electron.
+
+![](demo.png)
+
+It also tweaks a few things, like removing the obnoxious "Looking for the old version?" prompt from the top menu.
+
+## Installation
+
+
+## Development Setup
+
+1. Install [Node.js](https://nodejs.org/).
+2. Install the depedencies, and start the app:
+
+```sh
+npm install
+npm run start
+```
+
+## Compiling
+
+### OS X
+OS X has the advantage of being able to compile the OS X, Windows and Linux version of the app:
+
+1. Install [Homebrew](http://brew.sh/)
+2. Install `Wine` and `Makensis` which is needed for `electron-builder`.
+```sh
+brew install wine makensis
+```
+3. Install `electron-packager` and `electron-builder`.
+```sh
+npm install electron-packager --global && npm install electron-builder@1.1.0 --global
+```
+4. Run the build script for compiling all the binaries:
+```sh
+npm run pack:all
+```
+This will create a Windows installer, and create an OS X .dmg installer. (No Linux installer). The compiled binaries can be found in the releases folder.
+
+If you just want to create a windows installer, or just an OS X installer, use these commands:
+
+**Windows**
+```sh
+npm run pack:win
+```
+**OS X**
+```sh
+npm run pack:osx
+```
+### Windows
+Windows can only compile binaries for Windows.
+1. Install `electron-packager` and `electron-builder`.
+```sh
+npm install electron-packager --global && npm install electron-builder@1.1.0 --global
+```
+2. Run the build script for compiling the windows version:
+```sh
+npm run pack:win
+```
+
+### Linux
+Linux can compile binaries for Linux and Windows.
+1. Install `electron-packager` and `electron-builder`.
+```sh
+npm install electron-packager --global && npm install electron-builder@1.1.0 --global
+```
+2. Run the build script for compiling the linux and windows version:
+```sh
+npm run pack:win && npm run build:linux
+```
+## Contributing?
+Want to contribute? Great!
+If you found an issue, related to the app itself and not Adobe Story, submit it!
+Want to contribute code to the project? Just make a pull request, and if you´re code checks out we´ll merge it!
+
+## License
+[MIT](LICENSE.md)
---
+++ app/main.js
@@ -0,0 +1,84 @@
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 1000, height: 800, icon: __dirname + 'assets/sy_icon.png', titleBarStyle: "hiddenInset"})
+  mainWindow.setMenu(null);
+
+  mainWindow.webContents.on('did-finish-load', function() {
+  mainWindow.webContents.insertCSS('.header-button-panel coral-shell-menubar-item:first-child { display: none; }')
+  if (process.platform == 'darwin') {
+    mainWindow.webContents.insertCSS('#header-shell { -webkit-app-region: drag } .coral-Shell-header-home { margin-left: 70px !important; margin-right: 20px !important;} .coral-TabList { margin-left: 40px !important; }');
+  } 
+ });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('https://story.adobe.com', {userAgent: 'Safari'});
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools()
+
+  mainWindow.on('enter-full-screen', function () {
+    if (process.platform == 'darwin') {
+    mainWindow.webContents.insertCSS('#header-shell { -webkit-app-region: drag } .coral-Shell-header-home { margin-left: 0px !important; margin-right: 0px !important;} .coral-TabList { margin-left: 0px !important; }');
+    }
+  })
+
+  mainWindow.on('leave-full-screen', function () {
+    if (process.platform == 'darwin') {
+    mainWindow.webContents.insertCSS('#header-shell { -webkit-app-region: drag } .coral-Shell-header-home { margin-left: 70px !important; margin-right: 20px !important;} .coral-TabList { margin-left: 40px !important; }');
+    }
+  }) 
+
+  mainWindow.webContents.on('new-window', function(event, urlToOpen) {
+    event.preventDefault();
+    var newWindow = new BrowserWindow({width: 800, height: 600})
+    newWindow.loadURL(urlToOpen);
+  });
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+
+  require('./mainmenu')
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
